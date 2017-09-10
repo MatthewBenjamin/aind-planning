ef6bef4087165f6d175020a5ad6f7662b525b78a
Matthew Benjamin <matt.g.benjamin@gmail.com>
2017-09-10 15:47:15 -0500

    implement h_ignore_preconditions

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -225,9 +225,12 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
-        count = 0
-        return count
+        # DONE implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
+        goals = self.goal
+        goals_met = list(set(goals).intersection(kb.clauses))
+        return len(goals) - len(goals_met)
 
 
 def air_cargo_p1() -> AirCargoProblem:
